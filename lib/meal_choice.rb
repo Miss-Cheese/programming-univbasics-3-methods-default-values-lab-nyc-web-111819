ca38f6016a7c274a1e40be9d2db30c575c1a6edf
Natalie Galligan <nat.galligan@gmail.com>
2019-11-01 04:08:01 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,6 @@
 def meal_choice(veg1, veg2, protein ="meat")
   meal_order = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts "What a nutritious meal!" 
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal_order
 end
 
